86378f6c6e2da437bc6f8c86202f292d080eb2f1
Hugo Belém <belem.hugo@hotmail.com>
2024-08-22 17:49:35 -0300

    fixed typo in views.index

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request,question_id):
     return HttpResponse(f'You are looking at question {question_id}')
