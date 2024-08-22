e4f04e9a56251d2a7159bb48095f81cac1a9aec9
Hugo Belém <belem.hugo@hotmail.com>
2024-08-22 17:57:29 -0300

    added spaces

--- polls/views.py
+++ polls/views.py
@@ -6,6 +6,7 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
+
     return render(request, 'polls/index.html', context)
 
 def detail(request,question_id):
@@ -18,7 +19,9 @@ def detail(request,question_id):
 
 def results(request, question_id):
     response = f'You are looking at the results of the question {question_id}'
+
     return HttpResponse(response)
 
 def vote(request, question_id):
+    
     return HttpResponse(f'You are voting on question {question_id}')
