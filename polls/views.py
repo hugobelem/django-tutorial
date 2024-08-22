364c8fd7880f402a348fb53b92dcd5b67160de1e
Hugo Belém <belem.hugo@hotmail.com>
2024-08-22 20:18:33 -0300

    updated vote view function

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -22,5 +24,17 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
 
-    return HttpResponse(f'You are voting on question {question_id}')
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice',
+        })
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
