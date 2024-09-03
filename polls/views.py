481d58ae69d300915a6cfb5cab8ccf481d8f138d
Hugo Belém <belem.hugo@hotmail.com>
2024-09-03 12:04:44 -0300

    updated tests

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,7 +13,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date___lte=timezone.now()).order_by(
+            '-pub_date')[:5]
     
 
 
