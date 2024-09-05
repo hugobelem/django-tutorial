05a33c6ee6a07719942b60ed9f1d7b8213a1328b
Hugo Belém <belem.hugo@hotmail.com>
2024-09-05 13:25:38 -0300

    tests & views updated

--- polls/views.py
+++ polls/views.py
@@ -11,12 +11,17 @@ from .models import Question, Choice
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-    
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
     def get_queryset(self):
-        return Question.objects.filter(pub_date___lte=timezone.now()).order_by(
-            '-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
     
 
 
@@ -24,8 +29,11 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-    def get_queryset(self) -> QuerySet[Any]:
-        return Question.objects.filter(pub_date___lte=timezone.now())
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
     
 
 class ResultsView(generic.DetailView):
